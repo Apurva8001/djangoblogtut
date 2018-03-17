e5802884fc64037cdb4c55e4ef497db107e50122
Your Name <ajibolaolayanju@gmail.com>
2018-03-17 09:04:12 +0000

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,11 +126,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-efs_ROOT = os.path.dirname(os.path.abspath(__file__))
+mysite_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-MEDIA_ROOT = os.path.join(efs_ROOT, 'media')
+MEDIA_ROOT = os.path.join(mysite_ROOT, 'media')
 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' 'static/' subdirectories and in STATICFILES_DIRS.
 # Example: '/home/media/media.lawrence.com/static/'
-STATIC_ROOT = os.path.join(efs_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(mysite_ROOT, 'staticfiles')
 
 # URL prefix for static files.
 # Example: 'http://media.lawrence.com/static/'
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 # )
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(efs_ROOT, 'static'),
+    os.path.join(mysite_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
